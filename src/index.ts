0ce65d67664919de9822e89e3a8844e77b93e1c3
Muhsin Ahadi <muhsinahadi25@gmail.com>
2024-01-22 14:33:36 +0700

    edit cors settings

--- src/index.ts
+++ src/index.ts
@@ -21,9 +21,7 @@ createConnection().then(async () => {
     const app = express();
     app.use(express.json());
     app.use(cookieParser())
-    app.use(cors({
-        origin: ['http://localhost:3000', 'http://localhost:8080']
-    }));
+    app.use(cors());
 
     routes(app);
     app.listen(8000, ()=> {
